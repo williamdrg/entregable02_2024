8361999ebbc8e4981245052908aa5a9114e52759
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-07-28 16:44:45 -0500

    terminación del crud de usuarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const userRouter = require('./user.routes');
 const router = express.Router();
 
-// colocar las rutas aquí
-
+router.use('/users', userRouter)
 
 module.exports = router;
